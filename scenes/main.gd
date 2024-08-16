6b3d0c8a7d220eba3ff6bd0243272c6ad84927af
Held Grijo <held.grijo@gmail.com>
2024-08-16 00:13:46 -0400

    feat: remove creeps ao reiniciar a gameplay

--- scenes/main.gd
+++ scenes/main.gd
@@ -17,14 +17,15 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
-	
 	
 func _on_score_timer_timeout():
 	score += 1
